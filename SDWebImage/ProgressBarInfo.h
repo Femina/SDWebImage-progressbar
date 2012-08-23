7bd6dbf5413abdea7fc926f2cae4e62a72910b7b
Konstantinos Vaggelakos <kozze89@gmail.com>
2012-08-24 00:16:26 +0200

    Added class for configuring the progress bar. Also added a property to the DDProgressView to handle the inner spacing.

--- SDWebImage/ProgressBarInfo.h
+++ SDWebImage/ProgressBarInfo.h
@@ -7,7 +7,17 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @interface ProgressBarInfo : NSObject
+{
+    
+}
+
+@property (nonatomic, retain) UIColor *innerColor, *outerColor;
+@property (assign) CGRect frame;
+@property (readwrite) float innerspacing;
+@property (readwrite) bool hidden;
+
 
 @end
--- SDWebImage/ProgressBarInfo.m
+++ SDWebImage/ProgressBarInfo.m
@@ -10,4 +10,20 @@
 
 @implementation ProgressBarInfo
 
+@synthesize innerColor, outerColor;
+@synthesize frame;
+@synthesize innerspacing;
+@synthesize hidden;
+
+
+- (id)init
+{
+    self.frame = CGRectMake(100, 100, 100, 5);
+    self.innerColor = [UIColor lightGrayColor];
+    self.outerColor = [UIColor grayColor];
+    self.innerspacing = 1.5;
+    self.hidden = NO;
+    return self;
+}
+
 @end
